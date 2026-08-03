f19083cfa86490d9321f5e63b334cfd120b2757f
Gustavo L. M. de Oliveira <gustavo.l.m.de.oliveira@scania.com>
2026-08-03 23:22:37 +0200

    first api commit

---
+++ cmd/api/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/gustavolmo/portfolio-v2-api/internal/health"
+)
+
+var allowedOrigins = map[string]bool{
+	"http://localhost:5173":                              true,
+	"https://gustavolmo.github.io/react-window-manager/": true,
+}
+
+func cors(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		origin := r.Header.Get("Origin")
+
+		if allowedOrigins[origin] {
+			w.Header().Set("Access-Control-Allow-Origin", origin)
+			w.Header().Set("Vary", "Origin")
+			w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE, OPTIONS")
+			w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+		}
+
+		if r.Method == http.MethodOptions {
+			w.WriteHeader(http.StatusNoContent)
+			return
+		}
+
+		next.ServeHTTP(w, r)
+	})
+}
+
+func main() {
+	router := http.NewServeMux()
+
+	router.HandleFunc("GET /health", health.Hanlder)
+
+	log.Println("Server running on http://localhost:8080")
+
+	if err := http.ListenAndServe(":8080", cors(router)); err != nil {
+		log.Fatal(err)
+	}
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/gustavolmo/portfolio-v2-api
+
+go 1.26.5
